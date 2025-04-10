d935157590905483cf44ca711a5865cfe71eda3c
lorenzo-cmyk <10797603@polimi.it>
2025-04-10 10:50:43 +0200

    style: Broken lines exceeding length.

--- sources/controller.py
+++ sources/controller.py
@@ -25,7 +25,8 @@ class MessageFactory:
         ofparser = switch.ofproto_parser
 
         # Default configuration for the switch: sends everything to the controller.
-        # Instructions: for each packet, apply the action: send everything to the controller using the OFPP_CONTROLLER port.
+        # Instructions: for each packet, apply the action: send everything to the controller using
+        # the OFPP_CONTROLLER port.
         instructions = [
             ofparser.OFPInstructionActions(
                 ofprotocol.OFPIT_APPLY_ACTIONS,
@@ -36,7 +37,8 @@ class MessageFactory:
                 ],
             )
         ]
-        # Rule: match everything and apply the instructions. Keep the priority low to avoid conflicts with other rules.
+        # Rule: match everything and apply the instructions. Keep the priority low to avoid
+        # conflicts with other rules.
         rule = ofparser.OFPFlowMod(
             datapath=switch,
             priority=0,
@@ -49,7 +51,8 @@ class MessageFactory:
 
     def arp_proxy(self, arp_req):
         """
-        Generates a FlowMod message to reply to an ARP request with the MAC address of the host that has the IP address specified in the ARP request.
+        Generates a FlowMod message to reply to an ARP request with the MAC address of the host
+        that has the IP address specified in the ARP request.
         :param app: The Ryu application instance.
         :param arp_req: The ARP request packet received by the controller.
         :return: The FlowMod message to be sent to the switch.
@@ -60,7 +63,8 @@ class MessageFactory:
         ofprotocol = switch.ofproto
         ofparser = switch.ofproto_parser
 
-        # Parse the ARP request: starts from the raw byte stream received by the controller and extracts Ethernet and ARP informations.
+        # Parse the ARP request: starts from the raw byte stream received by the controller and
+        # extracts Ethernet and ARP informations.
         raw_in = packet.Packet(arp_req.data)
         eth_in = raw_in.get_protocol(ethernet.ethernet)
         arp_in = raw_in.get_protocol(arp.arp)
@@ -80,14 +84,20 @@ class MessageFactory:
 
         # Starts building the ARP reply packet.
         raw_out = packet.Packet()
-        # External Ethernet header: the destination MAC address is the source MAC address of the ARP request, the source MAC address is the MAC address of the host that has the IP address specified in the ARP request.
+        # External Ethernet header: the destination MAC address is the source MAC address of the
+        # ARP request, the source MAC address is the MAC address of the host that has the IP
+        # address specified in the ARP request.
         eth_out = ethernet.ethernet(
             dst=eth_in.src,
             src=target_mac_address,
             # Ethernet type: ARP
             ethertype=ether_types.ETH_TYPE_ARP,
         )
-        # ARP header: the opcode is ARP_REPLY, the source MAC address is the MAC address of the host that has the IP address specified in the ARP request, the source IP address is the IP address specified in the ARP request, the destination MAC address is the source MAC address of the ARP request, and the destination IP address is the IP address specified in the ARP request.
+        # ARP header: the opcode is ARP_REPLY, the source MAC address is the MAC address of the
+        # host that has the IP address specified in the ARP request, the source IP address is the
+        # IP address specified in the ARP request, the destination MAC address is the source MAC
+        # address of the ARP request, and the destination IP address is the IP address specified in
+        # the ARP request.
         arp_out = arp.arp(
             opcode=arp.ARP_REPLY,
             src_mac=target_mac_address,
@@ -99,7 +109,8 @@ class MessageFactory:
         raw_out.add_protocol(arp_out)
         raw_out.serialize()
 
-        # Build the FlowMod message to inscruct the switch to send the ARP reply that we have just built.
+        # Build the FlowMod message to inscruct the switch to send the ARP reply that we have just
+        # built.
         arp_reply = ofparser.OFPPacketOut(
             datapath=switch,
             buffer_id=ofprotocol.OFP_NO_BUFFER,
@@ -122,7 +133,8 @@ class MessageFactory:
         # Retrive the OpenFlow parser object from the switch.
         ofparser = switch.ofproto_parser
 
-        # Build the FlowMod message to instruct the switch to forward the packet to the specified output port.
+        # Build the FlowMod message to instruct the switch to forward the packet to the specified
+        # output port.
         # Actions: just forward the packet to said port.
         actions = [ofparser.OFPActionOutput(out_port)]
         # PacketOut: pack everything in a PacketOut message to be sent to the switch.
@@ -130,7 +142,8 @@ class MessageFactory:
             datapath=switch,
             buffer_id=pkt.buffer_id,
             # Spoof the port to make it look like the packet is coming from the switch.
-            # This is needed because the packet is coming from the controller and not from the switch.
+            # This is needed because the packet is coming from the controller and not from the
+            # switch.
             in_port=pkt.match["in_port"],
             actions=actions,
             data=pkt.data,
@@ -163,7 +176,8 @@ class NetworkTopology:
 
     def __find_next_hop_port(self, src_switch_id, dst_switch_id):
         """
-        Finds the port which connects the source switch to the next hop switch in the path to the destination switch.
+        Finds the port which connects the source switch to the next hop switch in the path to the
+        destination switch.
         :param src_switch_id: The ID of the source switch.
         :param dst_switch_id: The ID of the destination switch.
         :return: The port number of the next hop switch.
@@ -185,7 +199,8 @@ class NetworkTopology:
 
     def find_output_port(self, src_switch, dst_mac):
         """
-        Finds the output port to which the packet should be sent based on the destination MAC address.
+        Finds the output port to which the packet should be sent based on the destination MAC
+        address.
         :param src_switch: The object representing the source switch.
         :param dst_mac: The destination MAC address.
         :return: The output port number.
@@ -197,7 +212,8 @@ class NetworkTopology:
         if dst_switch_id is None:
             return None
 
-        # If the host is directly connected to the source switch, return the port number of the host.
+        # If the host is directly connected to the source switch, return the port number of the
+        # host.
         if dst_switch_id == src_switch.id:
             return dst_switch_port
 
@@ -256,7 +272,8 @@ class BabyElephantWalk(app_manager.RyuApp):
         if eth_in.ethertype != ether_types.ETH_TYPE_IP:
             return
 
-        # If the packet is an IPv4 packet, find the output port to which the packet should be sent based on the destination MAC address.
+        # If the packet is an IPv4 packet, find the output port to which the packet should be sent
+        # based on the destination MAC address.
         output_port = self.network_topology.find_output_port(switch, eth_in.dst)
         if output_port is None:
             return
