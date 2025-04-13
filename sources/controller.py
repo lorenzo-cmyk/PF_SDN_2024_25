227e3bc951f3305032f4b1a7c3ee114a125e4da4
lorenzo-cmyk <10797603@polimi.it>
2025-04-13 16:00:50 +0200

    feat: Update TCP stream volume threshold to 25 MB for improved performance.

--- sources/controller.py
+++ sources/controller.py
@@ -13,7 +13,7 @@ import networkx as nx
 TCP_FORWARDING_RULE_PRIORITY = 100
 # Threshold for the TCP stream volume: if the volume is greater than this value, a rule will be
 # installed to forward the TCP stream directly bypassing the controller. Value is in bytes.
-TCP_STREAM_VOLUME_THRESHOLD = 500000
+TCP_STREAM_VOLUME_THRESHOLD = 25 * 1000 * 1000 # 25 MB
 # Timeout for the TCP forwarding rules: if the connection is not used for this amount of time, it
 # will be removed from the switch or from the controller. Value is in seconds.
 TCP_CONNECTION_TIMEOUT = 20
