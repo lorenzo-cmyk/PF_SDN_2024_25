f2b72bde400c19880caadc72b23a0d4d833cc415
lorenzo-cmyk <10797603@polimi.it>
2025-04-24 23:44:31 +0200

    feat: Add Flow Manager support to ryu-manager launch script and update Mininet script documentation.

--- start_network.sh
+++ start_network.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# This script starts Mininet with the custom topology specified. The controller flag enables the
+# use of an external controller: Ryu.
+
 mn --custom ./src/network/main.py --topo SDNTestbed --controller=remote
